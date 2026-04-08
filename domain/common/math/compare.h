b56f3404a1a7d0607d3e1414f4fb7eaf8437c080
minstrel9264 <minsrel9264@gmail.com>
2026-04-08 18:43:59 +0800

    1000020-renamed-files

--- domain/common/math/compare.h
+++ domain/common/math/compare.h
@@ -4,15 +4,19 @@
 
 #ifndef DOMAIN_COMMON_MATH_COMPARE_H
 #define DOMAIN_COMMON_MATH_COMPARE_H
-
+#include <cmath>
 namespace domain {
 namespace common {
 namespace math {
 bool doubleLess(const double a,const double b,const double precision = 1e-9) {
   return a - b < precision;
 }
-bool doubleLessEqual(const double a,const double b,const double precision =1e-9) {
+bool doubleLessEqual(const double a,const double b,const double precision =
+  1e-9) {
   return a - b <= precision;
+}
+bool doubleEqual(const double a,const double b,const double precision = 1e-9) {
+  return std::fabs(a - b) < precision;
 }
 }
 }
